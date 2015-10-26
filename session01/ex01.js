7238d1e630112ec1a5e694d4d316338bec7b1a98
Alex Wijns <awijns@gmail.com>
2015-10-26 21:39:27 +0000

    Add exercise instructions for session 1

---
+++ session01/ex01.js
@@ -0,0 +1,39 @@
+// part 1: strings
+
+// define two variables and assign strings to them
+
+// define a third variable which holds the concatenation of the two other strings
+
+// write a function which does exactly this, accepts two strings and returns their concatenation
+
+// use this function to define another function which accepts a name and outputs "Hello name"
+
+// Was it useful to define the concat function?
+
+
+// part 2: numbers
+
+// define variables x and y
+
+// assign a number to both variables
+
+// compute the square of both numbers, store the results in variables x2 and y2
+
+// compute the square root of the sum of x2 and y2 and display it in the console
+
+// you just computed the length of a line in the plane from (0,0) to (x,y)
+// write a function length which does exactly this, 
+// so that the previous operations can be captured by length(5, 6)
+
+
+// part 3: p5.js
+
+//write the following in a file
+
+function setup() {
+
+}
+
+// go to the website p5js.org and figure out which function to call to draw a circle on the screen
+// call that function in setup()
+// can you make the circle move??
