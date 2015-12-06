e2bbbf9e4d44105f026cd189fa1b08e23ebabd63
Alex Wijns <awijns@gmail.com>
2015-12-06 18:25:06 +0000

    Update first p5 sketch

--- session02/sketch.js
+++ session02/sketch.js
@@ -1,21 +1,26 @@
 // First p5.js program
 
 // define variables
-var x;
+var x, y,  speedX = 3, speedY = 2;
 
 // setup is called once at the start of the program
 function setup() {
-	createCanvas(200, 200); // set width and height of drawing canvas
+	createCanvas(windowWidth, windowHeight); // set width and height of drawing canvas
 	x = width/2;
+	y = height/2;
 }
 
 // draw is called 60 times per second (frameRate) by default
 function draw() {
-	background(150, 0, 0); // arguments: RGB color values
-	ellipse(x, 50, 50, 50); // arguments: x, y, width, height
-	x = x + 1;
+	background(0); // arguments: RGB color values
+	ellipse(x, y, 50, 50); // arguments: x, y, width, height
+	x = x + speedX;
+	y = y + speedY
 
-	if (x > width) {
-		x = 0;
+	if (x > width || x < 0) {
+		speedX = - speedX;
+	}
+	if (y > height || y < 0) {
+		speedY = - speedY;
 	}
 }
